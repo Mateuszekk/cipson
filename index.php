0a2af21de4575f810362bff4a75326f34b024038
Mateuszekk <46939637+Mateuszekk@users.noreply.github.com>
2020-08-16 16:14:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            color: black;
+        }
+    </style>
+
+</head>
+<body>
+    <nav>
+        <a href="index.php" class="menu">home</a>
+        <a href="artists.php" class="menu">artists</a>
+    </nav>
+
+    <input type="text">
+    <input type="button" value="search">
+</body>
+</html>
